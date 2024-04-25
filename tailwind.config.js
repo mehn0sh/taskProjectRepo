8519b780c019fec2594749e5873242cbedb842ea
mahnoosh <mahnooshekrami@gmai.com>
2024-04-25 22:21:43 +0330

    style-change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+     fontFamily:{
+      sans: ["Vazir", ...fontFamily.sans],
+
+     }
+    },
   },
   plugins: [],
 }
