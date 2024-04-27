0ea0a472582e4ed2872ff17f7630eb4b15c3520b
mahnoosh <mahnooshekrami@gmai.com>
2024-04-28 02:26:10 +0330

    implement loader

---
+++ src/ui/Loader/LoaderComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../Loader/Loader.css";
+const LoaderComponent = () => {
+  return (
+    <div className="w-full flex ">
+      <span className="loader m-auto"></span>
+    </div>
+  );
+};
+
+export default LoaderComponent;
